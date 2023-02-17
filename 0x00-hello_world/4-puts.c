fc6a1db9c7367c9a5674dc0fd3a02f762a55f2f7
Nomhlewethu <nomhlewethu@gmail.com>
2023-02-16 16:57:13 -0800

    We are coding C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programme that prints with puts instead of printf
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A programme thats prints out the various types
+ * Return 0 (success)
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+printf("Size of a char: %lu byte(s)/n"), (sizeof(a));
+printf("Size of a char: %lu byte(s)/n"), (sizeof(b));
+printf("Size of a char: %lu byte(s)/n"), (sizeof(c));
+printf("Size of a char: %lu byte(s)/n"), (sizeof(d));
+printf("Size of a char: %lu byte(s)/n"), (sizeof(e));
+return (0);
+}
