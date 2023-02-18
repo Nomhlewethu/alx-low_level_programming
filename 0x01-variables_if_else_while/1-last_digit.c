33aae9903a4c05a9eb082a8edaad6b7cb31aeb7a
Nomhlewethu <nomhlewethu@gmail.com>
2023-02-17 16:48:46 -0800

    If else and while

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entyr
+ * Return: Always 0
+ */
+int main(void)
+{
+int n;
+int a;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+n = n % 10;
+if (a > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", a);
+}
+else if (a == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, a);
+}
+else
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
+}
+return (0);
+}
